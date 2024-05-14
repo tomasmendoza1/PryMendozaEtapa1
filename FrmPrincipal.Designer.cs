dd14691400dc3180057446e661f54072269e4dc5
Alumnos <Alumnos@PC143>
2024-05-14 12:10:33 -0300

    Agregar archivos de proyecto.

---
+++ FrmPrincipal.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace PryMendozaEtapa1
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ptcRegistrarFirmar = new System.Windows.Forms.PictureBox();
+            this.BtnComenzar = new System.Windows.Forms.Button();
+            this.BtnGuardar = new System.Windows.Forms.Button();
+            this.BtnCerrar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ptcRegistrarFirmar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ptcRegistrarFirmar
+            // 
+            this.ptcRegistrarFirmar.Location = new System.Drawing.Point(12, 12);
+            this.ptcRegistrarFirmar.Name = "ptcRegistrarFirmar";
+            this.ptcRegistrarFirmar.Size = new System.Drawing.Size(575, 247);
+            this.ptcRegistrarFirmar.TabIndex = 0;
+            this.ptcRegistrarFirmar.TabStop = false;
+            this.ptcRegistrarFirmar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ptcRegistrarFirmar_MouseDown);
+            this.ptcRegistrarFirmar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ptcRegistrarFirmar_MouseMove);
+            this.ptcRegistrarFirmar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ptcRegistrarFirmar_MouseUp);
+            // 
+            // BtnComenzar
+            // 
+            this.BtnComenzar.Location = new System.Drawing.Point(12, 265);
+            this.BtnComenzar.Name = "BtnComenzar";
+            this.BtnComenzar.Size = new System.Drawing.Size(178, 34);
+            this.BtnComenzar.TabIndex = 1;
+            this.BtnComenzar.Text = "Comezar";
+            this.BtnComenzar.UseVisualStyleBackColor = true;
+            this.BtnComenzar.Click += new System.EventHandler(this.BtnComenzar_Click);
+            // 
+            // BtnGuardar
+            // 
+            this.BtnGuardar.Location = new System.Drawing.Point(398, 265);
+            this.BtnGuardar.Name = "BtnGuardar";
+            this.BtnGuardar.Size = new System.Drawing.Size(189, 34);
+            this.BtnGuardar.TabIndex = 2;
+            this.BtnGuardar.Text = "Guardar";
+            this.BtnGuardar.UseVisualStyleBackColor = true;
+            this.BtnGuardar.Click += new System.EventHandler(this.BtnGuardar_Click);
+            // 
+            // BtnCerrar
+            // 
+            this.BtnCerrar.Location = new System.Drawing.Point(196, 266);
+            this.BtnCerrar.Name = "BtnCerrar";
+            this.BtnCerrar.Size = new System.Drawing.Size(196, 34);
+            this.BtnCerrar.TabIndex = 3;
+            this.BtnCerrar.Text = "Cerrar";
+            this.BtnCerrar.UseVisualStyleBackColor = true;
+            this.BtnCerrar.Click += new System.EventHandler(this.BtnCerrar_Click);
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(599, 312);
+            this.Controls.Add(this.BtnCerrar);
+            this.Controls.Add(this.BtnGuardar);
+            this.Controls.Add(this.BtnComenzar);
+            this.Controls.Add(this.ptcRegistrarFirmar);
+            this.Name = "FrmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Etapa 1 - DIBUJAR";
+            this.Load += new System.EventHandler(this.FrmPrincipal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ptcRegistrarFirmar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ptcRegistrarFirmar;
+        private System.Windows.Forms.Button BtnComenzar;
+        private System.Windows.Forms.Button BtnGuardar;
+        private System.Windows.Forms.Button BtnCerrar;
+    }
+}
+
---
+++ FrmPrincipal.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PryMendozaEtapa1
+{
+    public partial class FrmPrincipal : Form
+    {
+        public Bitmap mousemap; //public bit map 
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+            mousemap = new Bitmap(ptcRegistrarFirmar.Width, ptcRegistrarFirmar.Height); // bit map = ptc location
+            ptcRegistrarFirmar.Image = mousemap;
+        }
+
+        bool EstadoDeMouse;//up y down
+        Point CordenadasDelFormulario;
+
+        private void BtnComenzar_Click(object sender, EventArgs e)
+        {
+            BtnComenzar.Enabled = false; //desactivar
+            BtnGuardar.Enabled = true;   //activar
+        }
+
+        private void ptcRegistrarFirmar_MouseDown(object sender, MouseEventArgs e)
+        {
+            EstadoDeMouse = true; //activar
+            CordenadasDelFormulario = e.Location; // e location = donde esta el mouse 
+        }
+
+        private void ptcRegistrarFirmar_MouseUp(object sender, MouseEventArgs e)
+        {
+            EstadoDeMouse = false;
+        }
+
+        private void ptcRegistrarFirmar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (EstadoDeMouse == true)
+            {
+                using(Graphics g = ptcRegistrarFirmar.CreateGraphics())
+                {
+                    using(Pen ObjetoLapiz = new Pen(Color.Black, 3))
+                    {
+                        g.DrawLine(ObjetoLapiz, CordenadasDelFormulario, e.Location);
+                        CordenadasDelFormulario = e.Location;
+                    }
+                }
+            }
+        }
+
+        private void BtnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BtnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string RutaDeFirmas = Path.Combine(Application.StartupPath, "FirmasRegistradas"); //carpeta 
+                if (!Directory.Exists(RutaDeFirmas))
+                {
+                    Directory.CreateDirectory(RutaDeFirmas);
+                }
+                string archivo = $"{DateTime.Now.ToString("yyyy-MM-dd hhmmss")}.png"; //dia mes año segundos 
+                string destino = Path.Combine(RutaDeFirmas, archivo); //
+                mousemap.Save(destino, System.Drawing.Imaging.ImageFormat.Png); //formato 
+                MessageBox.Show("Ha registrado con éxito su firma!");
+                ptcRegistrarFirmar.Invalidate();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("error" + ex.Message);
+            }
+        }
+        private void FrmPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
